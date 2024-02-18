2af963783301e57c7cb2043ed38a20e6357eb830
vvasylykha <vvasylykha@chainfulness.com>
2024-02-18 13:48:04 +0200

    init project structure

---
+++ src/main/java/org/decardo/Decardo.java
@@ -0,0 +1,16 @@
+package org.decardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vitalii Vasylykha
+ * @company UzhNU
+ * @since 2024/02/18
+ */
+@SpringBootApplication
+public class Decardo {
+	public static void main(String[] args) {
+		SpringApplication.run(Decardo.class, args);
+	}
+}
